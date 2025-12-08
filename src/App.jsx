fd1dfb589f5c621a7bf21ed0ec29370dea55d0a9
zakia <zoyabibi536@gmail.com>
2025-12-08 22:22:45 +0500

    Update App.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import './App.css'
 import Header from './components/Header'; 
 import User from './components/Footer'; 
  import Profile from './components/profile';
-//  import Employee from './components/Employee';
+ import Employee from './components/employee';
 // import Student from './components/studentcard';
 // let studentid=1;
 function App() {
 
   return (
     <div>
-    {/* <Employee id={[3,1]} /> */}
-    <Profile id="3"studentinfo="student information"/> 
+     <Employee/> 
+    {/* <Profile  maxPrice={20}/>  */}
 {/* <Student studentinfo="Student ID Card" /> */}
 
     {/* <Profile  /> */}
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,31 +1,31 @@
 function Profile (props) {
-
+let Product=[
+  {product_id:1,title:"English", price:22,category:"IT",description:"i love coding"},
+   {product_id:2,title:"Urdu", price:24,category:"CS",description:"i love coding"},
+    {product_id:3,title:"Atomic Habits", price:32,category:"math",description:"i love math"}
+]
   // 1: Create list of student objects
-  let students = [
-    {studentid: 1, name: "Zara", phone: "099988888", bio: "I am Zara, CS student." },
-    { studentid: 2, name: "Ali", phone: "033322211", bio: "I love coding." },
-    { studentid: 3, name: "Sara", phone: "030012345", bio: "Frontend developer." }
-  ];
-let res=students.find((std)=>{
- return String(std.studentid)==props.id
-})
- return (
-   <div className="parent"> 
- { res ?(
- <div key={res.studentid} className="card">
-              <h2>ID: {res.studentid}</h2>
-              <h3>Name: {res.name}</h3>
-              <p>Phone: {res.phone}</p>
-              <p>Bio: {res.bio}</p>
-            </div>
- )
-       
-    :("not found")    
+let res=Product.filter((pro)=>{
+  return pro.price>props.maxPrice
+}) 
+return(
+  <div>
+  {
+    res.map((pro,index)=>{
+      return(
+        <div>
+    <h2 key={index}>Product ID: {pro.product_id}</h2>
+    <h2>Name:{pro.title}</h2>
+    <h2>Price: {pro.price}</h2>
+      <h2>Category:{pro.category}</h2>
+ </div>
+      )
+        })
       }
-      
-      </div>
-    
-  )
-}
 
+  
+  </div>
+)
+
+}
 export default Profile;
